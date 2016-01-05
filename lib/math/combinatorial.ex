a3ac58bb4d91583ed6117890c1cd69f168f505f8
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2016-01-04 21:56:39 -0600

    Added factorial/1; optimized choose/1; now testing Elixir 1.2.0

--- lib/math/combinatorial.ex
+++ lib/math/combinatorial.ex
@@ -4,6 +4,7 @@
 # as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
 
 defmodule Math.Combinatorial do
+
   @spec cartesian_product(list, list) :: [tuple]
   def cartesian_product(coll1, coll2) do
     for item1 <- coll1, item2 <- coll2, do: {item1, item2}
@@ -13,21 +14,29 @@ defmodule Math.Combinatorial do
   def choose([], _k) do
     []
   end
-  def choose(items, 1) when is_list(items) do
-    for item <- items, do: [item]
+  def choose(items, 1) when is_list items do
+    Enum.map items, &([&1])
   end
   def choose([head|tail], k) do
-    (for subchoose <- choose(tail, k - 1) do
+    Enum.map(choose(tail, k - 1), fn subchoose ->
       [head|subchoose]
     end) ++ choose(tail, k)
   end
 
   @spec choose(pos_integer, pos_integer) :: pos_integer
   def choose(n, k) when n > 0 do
-    1..n
-    |> Enum.to_list
-    |> choose(k)
-    |> length
+    div factorial(n), factorial(k) * factorial(n - k)
+  end
+
+  @spec factorial(non_neg_integer) :: pos_integer
+  def factorial(0) do
+    1
+  end
+  def factorial(1) do
+    1
+  end
+  def factorial(n) when is_integer(n) and n > 1 do
+    Enum.reduce 2..n, 1, &(&1 * &2)
   end
 
   @spec permute(list) :: [list]
