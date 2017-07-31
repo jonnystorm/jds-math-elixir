56891fe00f4fc5ba6a71637e8036fae5d8f8d74d
Jonathan D. Storm <jds@idio.link>
2017-07-31 14:48:31 -0500

    Eliminate Elixir warnings

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule JDSMath.Mixfile do
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps
+      deps: deps()
     ]
   end
 
