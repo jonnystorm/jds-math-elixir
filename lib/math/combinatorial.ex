4db6247774a8e33c23273456e5513f8d13087f2d
Jonathan D. Storm <jds@idio.link>
2017-09-02 22:32:10 -0500

    Reformat for readability

--- lib/math/combinatorial.ex
+++ lib/math/combinatorial.ex
@@ -4,48 +4,53 @@
 
 defmodule Math.Combinatorial do
 
-  @spec cartesian_product(list, list) :: [tuple]
+  @spec cartesian_product(list, list)
+    :: [tuple]
   def cartesian_product(coll1, coll2) do
-    for item1 <- coll1, item2 <- coll2, do: {item1, item2}
+    for item1 <- coll1,
+        item2 <- coll2,
+      do: {item1, item2}
   end
 
-  @spec choose(list, pos_integer) :: list
-  def choose([], _k) do
-    []
-  end
-  def choose(items, 1) when is_list items do
-    Enum.map items, &([&1])
-  end
+  @spec choose(list, pos_integer)
+    :: list
+  def choose([], _k),
+    do: []
+
+  def choose(items, 1) when is_list(items),
+    do: Enum.map(items, &([&1]))
+
   def choose([head|tail], k) do
-    Enum.map(choose(tail, k - 1), fn subchoose ->
-      [head|subchoose]
-    end) ++ choose(tail, k)
+    choose(tail, k - 1)
+    |> Enum.map(& [head|&1])
+    |> Enum.concat(choose(tail, k))
   end
 
-  @spec choose(pos_integer, pos_integer) :: pos_integer
-  def choose(n, k) when n > 0 do
-    div factorial(n), factorial(k) * factorial(n - k)
-  end
+  @spec choose(pos_integer, pos_integer)
+    :: pos_integer
+  def choose(n, k) when n > 0,
+    do: div(factorial(n), factorial(k) * factorial(n - k))
 
-  @spec factorial(non_neg_integer) :: pos_integer
-  def factorial(0) do
-    1
-  end
-  def factorial(1) do
-    1
-  end
-  def factorial(n) when is_integer(n) and n > 1 do
-    Enum.reduce 2..n, 1, &(&1 * &2)
-  end
+  @spec factorial(non_neg_integer)
+    :: pos_integer
+  def factorial(0),
+    do: 1
 
-  @spec permute(list) :: [list]
-  def permute([]) do
-    [[]]
-  end
+  def factorial(1),
+    do: 1
+
+  def factorial(n) when is_integer(n) and n > 1,
+    do: Enum.reduce(2..n, 1, &(&1 * &2))
+
+  @spec permute(list)
+    :: [list]
+  def permute([]),
+    do: [[]]
+
   def permute(items) do
-    for head <- items, tail <- permute(items -- [head]) do
-      [head|tail]
-    end
+    for head <- items,
+        tail <- permute(items -- [head]),
+      do: [head|tail]
   end
 end
 
--- lib/math/information.ex
+++ lib/math/information.ex
@@ -11,14 +11,20 @@ defmodule Math.Information do
     binary
     |> :binary.bin_to_list
     |> Enum.group_by(&(&1))
-    |> Enum.map(fn {key, list} -> {key, length list} end)
+    |> Enum.map(fn {key, list} ->
+      {key, length(list)}
+    end)
   end
 
   def shannon_entropy(binary) do
     binary
     |> count_characters
-    |> Enum.map(fn {char, count} -> {char, count / byte_size binary} end)
-    |> Enum.map(fn {_, probability} -> probability * log_2(probability) end)
+    |> Enum.map(fn {char, count} ->
+      {char, count / byte_size binary}
+    end)
+    |> Enum.map(fn {_, probability} ->
+      probability * log_2(probability)
+    end)
     |> Enum.sum
     |> (fn s -> -s end).()
   end
